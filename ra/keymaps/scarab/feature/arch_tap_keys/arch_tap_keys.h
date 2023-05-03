623a42dd46dcc713279aab85fd0c34f04c2f7ba6
AVONSTU1 <AVONSTU1@ford.com>
2023-05-03 13:52:44 -0400

    remove unnecessary nested dir

--- ra/keymaps/scarab/feature/arch_tap_keys/arch_tap_keys.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "quantum.h"
-#include "feature/feature_manager.h"
-
-bool macro_iterator_forward(uint16_t keycode, keyrecord_t *record, void *data);
-bool macro_iterator_backward(uint16_t keycode, keyrecord_t *record, void *data);
-bool track_key_press(uint16_t keycode, keyrecord_t *record);
